ed27e6615a7514e16f9f40e2dcbef7a2b41642a8
Marlon Aleman <mjaleman1997@gmail.com>
2021-01-27 20:19:31 -0800

    Restructure movie.py for readability

--- movie.py
+++ movie.py
@@ -1,12 +1,16 @@
 class Movie:
 
-	def __init__(self, name, release_year, budget, director, duration, rating):
-		self.name = name
+	def __init__(self, title, release_year, budget, director, duration, rating):
+		self.title = title
 		self.release_year = release_year
 		self.budget = budget
 		self.director = director
 		self.duration = duration
+                self.rating = rating
 
 	def description(self):
-		return f"{self.name} was released with a budget of ${self.budget} in {self.release_year}. It was directed by {self.director}"
+		return f"{self.name} was released with a budget of 
+            ${self.budget} in {self.release_year}.
+            It was directed by {self.director}"
+
 
