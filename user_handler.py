d3d62a7c29560b21e0cd89baea5063a670a933af
Marlon Aleman <mjaleman1997@gmail.com>
2021-01-29 22:39:51 -0800

    Correct movie query url

--- user_handler.py
+++ user_handler.py
@@ -6,9 +6,7 @@ def queryUser(username):
 
 def get_movie_url(movie):
     try:
-        return f"""https://api.themoviedb.org/3/search/movie?
-        api_key=ddc5407839c0e22afe0624dd5f3f4235&language=en-US&
-        query={movie}&page=1&include_adult=false"""
+        return f"""https://api.themoviedb.org/3/search/movie?api_key=ddc5407839c0e22afe0624dd5f3f4235&language=en-US&query={movie}&page=1&include_adult=false"""
     except:
         e = sys.exc_info()[0]
         return e
