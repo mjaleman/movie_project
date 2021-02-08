cf42c304f97a92fdccf7f5bd3efe5ab88f5b4feb
Marlon Aleman <mjaleman1997@gmail.com>
2021-02-08 00:06:26 -0800

    modify test.py for CastBuilder.py testing

--- test.py
+++ test.py
@@ -4,4 +4,7 @@ from CastBuilder import CastBuilder
 new_movie = Movie("Avatar", "2009", "7.5", 19995)
 print(new_movie)
 cast_instance = CastBuilder(new_movie)
-print(cast_instance.get_cast_link())
+cast_instance.get_cast_link()
+json_file = cast_instance.get_JSON_file()
+to_text = json_file.convert_to_readable()
+print(to_text)
