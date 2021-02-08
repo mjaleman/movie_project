6822300be3083b2ac3c331c12a086fa2076db41f
Marlon Aleman <mjaleman1997@gmail.com>
2021-02-07 22:39:32 -0800

    Add getters to Movie.py class

--- movie.py
+++ movie.py
@@ -1,8 +1,9 @@
 class Movie:
-    def __init__(self, title, release_year, rating):
+    def __init__(self, title, release_year, rating, movie_id):
         self.title = title
         self.release_year = release_year
         self.rating = rating
+        self.movie_id = movie_id
 
     def __repr__(self):
         string_representation = f"\n{self.title} -- {self.release_year} -- {self.rating}"
@@ -10,5 +11,17 @@ class Movie:
     
     def description(self):
         return f"{self.name} was released with a budget of ${self.budget} in {self.release_year}.It was directed by {self.director}"
+
+    def get_title(self):
+        return self.title
+
+    def get_release_year(self):
+        return self.release_year
+
+    def get_rating(self):
+        return self.rating
+
+    def get_movie_id(self):
+        return movie_id
 
 
