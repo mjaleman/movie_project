5afb99c21498532a5eb90461016d304eb3ba7cce
Marlon Aleman <mjaleman1997@gmail.com>
2021-02-07 22:35:33 -0800

    Add comments to MovieListbuilder. Correct filename for Movie.py

--- MovieListBuilder.py
+++ MovieListBuilder.py
@@ -6,14 +6,19 @@ from Movie import Movie
 class MovieListBuilder:
     movie_list = []
 
+    # Retrieves information from the API and returns a JSON file
     def retrieve_api_info(self):
         response = urllib.request.urlopen(self.url)
         return response.read()
 
+    # Converts JSON information into string format
     def json_information(self):
         convert_to_string = json.loads(self.retrieve_api_info())
         return convert_to_string
 
+    # Parses information to return Movie object containing the Title,
+    # release date, movie rating, and the movie's internal database
+    # ID
     def get_result_at_index(self, data, index):
         result_at_index = data[index]
         title = result_at_index['original_title']
@@ -23,9 +28,11 @@ class MovieListBuilder:
         movie = Movie(title, release_date, rating, movie_id)
         return movie
 
+    # Returns a string contianing the year the movie was released
     def get_year(self, full_date):
         return full_date[0:4]
 
+    # Generates a list of movies from user query
     def generate_movie_list(self):
         raw_data = self.json_information()
         all_results = raw_data['results']
@@ -33,6 +40,7 @@ class MovieListBuilder:
             movie_info = self.get_result_at_index(all_results, all_results.index(item))
             self.movie_list.append(movie_info)
 
+    # Returns list of Movies
     def get_movie_list(self):
         return self.movie_list
 
